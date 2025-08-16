3e624e3bffa9bcd67d211ee01a8cc2009eea831d
Eigen438 <>
2025-08-16 19:37:25 +0900

    add comment

--- fapi.go
+++ fapi.go
@@ -6,6 +6,8 @@ import (
 	"github.com/google/uuid"
 )
 
+// FapiResourceEndpointMiddleware is a middleware that handles the x-fapi-interaction-id header for Financial-grade API.
+// If the header is not present in the request, it generates a new UUID and adds it to the request and response headers.
 func FapiResourceEndpointMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		interactionId := r.Header.Get("x-fapi-interaction-id")
